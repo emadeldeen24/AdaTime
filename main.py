9ff5eca0385c765d906f4029a3aacfea3044598b
Bai Yeqi <981011byq@gmail.com>
2022-05-25 09:13:55 +0800

    Modify the default --data_path arg, in order to generalize better to machines other than windows

--- main.py
+++ main.py
@@ -17,7 +17,7 @@ parser.add_argument('--run_description',        default='test',            type=
 parser.add_argument('--da_method',              default='Deep_Coral',             type=str, help='DANN, Deep_Coral, WDGRL, MMDA, VADA, DIRT, CDAN, ADDA, HoMM, CoDATS')
 
 # ========= Select the DATASET ==============
-parser.add_argument('--data_path',              default=r'D:\Codes\AdaTime\data',           type=str, help='Path containing dataset')
+parser.add_argument('--data_path',              default='./data',           type=str, help='Path containing dataset')
 parser.add_argument('--dataset',                default='HAR',              type=str, help='Dataset of choice: (WISDM - EEG - HAR - HHAR_SA)')
 
 # ========= Select the BACKBONE ==============
