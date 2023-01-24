f666f9b530d2ed55dfbb07d28e09d4c2bf5323b0
Emadeldeen Eldele <37911596+emadeldeen24@users.noreply.github.com>
2023-01-24 14:12:47 +0800

    Update models.py

--- models/models.py
+++ models/models.py
@@ -4,6 +4,7 @@ import math
 from torch.autograd import Function
 from torch.nn.utils import weight_norm
 import torch.nn.functional as F
+from .resnet18 import resnet18
 
 
 # from utils import weights_init
@@ -50,29 +51,29 @@ class CNN(nn.Module):
 
         self.adaptive_pool = nn.AdaptiveAvgPool1d(configs.features_len)
 
-        # weights_init(self.conv_block1)
-        # weights_init(self.conv_block2)
-        # weights_init(self.conv_block3)
-
     def forward(self, x_in):
         x = self.conv_block1(x_in)
         x = self.conv_block2(x)
         x = self.conv_block3(x)
         x = self.adaptive_pool(x)
+
         x_flat = x.reshape(x.shape[0], -1)
         return x_flat
+
 
 
 class classifier(nn.Module):
     def __init__(self, configs):
         super(classifier, self).__init__()
-
-        model_output_dim = configs.features_len
-        self.logits = nn.Linear(model_output_dim * configs.final_out_channels, configs.num_classes)
+        self.logits = nn.Linear(configs.features_len * configs.final_out_channels, configs.num_classes)
+        self.configs = configs
 
     def forward(self, x):
+
         predictions = self.logits(x)
+
         return predictions
+
 
 
 ########## TCN #############################
@@ -166,46 +167,15 @@ class TCN(nn.Module):
 
 
 ######## RESNET ##############################################
+
 class RESNET18(nn.Module):
     def __init__(self, configs):
-        layers = [2, 2, 2, 2]
-        block = BasicBlock
-        self.inplanes = configs.input_channels
         super(RESNET18, self).__init__()
-        self.layer1 = self._make_layer(block, configs.mid_channels, layers[0], stride=configs.stride)
-        self.layer2 = self._make_layer(block, configs.mid_channels * 2, layers[1], stride=1)
-        self.layer3 = self._make_layer(block, configs.final_out_channels, layers[2], stride=1)
-        self.layer4 = self._make_layer(block, configs.final_out_channels, layers[3], stride=1)
-
-        self.avgpool = nn.MaxPool1d(kernel_size=2, stride=2, padding=1)
-        self.adaptive_pool = nn.AdaptiveAvgPool1d(configs.features_len)
-
-    def _make_layer(self, block, planes, blocks, stride=1):
-        downsample = None
-        if stride != 1 or self.inplanes != planes * block.expansion:
-            downsample = nn.Sequential(
-                nn.Conv1d(self.inplanes, planes * block.expansion,
-                          kernel_size=1, stride=stride, bias=False),
-                nn.BatchNorm1d(planes * block.expansion),
-            )
-        layers = []
-        layers.append(block(self.inplanes, planes, stride, downsample))
-        self.inplanes = planes * block.expansion
-        for i in range(1, blocks):
-            layers.append(block(self.inplanes, planes))
-        return nn.Sequential(*layers)
-
-    def forward(self, x):
-        x = self.layer1(x)
-        x = self.layer2(x)
-        x = self.layer3(x)
-        x = self.layer4(x)
-
-        x = self.adaptive_pool(x)
-
+        self.resnet = resnet18(configs)
+    def forward(self, x_in):
+        x = self.resnet(x_in)
         x_flat = x.reshape(x.shape[0], -1)
         return x_flat
-
 
 class BasicBlock(nn.Module):
     expansion = 1
@@ -381,3 +351,191 @@ class AdvSKM_Disc(nn.Module):
         out_rel = self.branch_2(input)
         total_out = torch.cat((out_cos, out_rel), dim=1)
         return total_out
+    
+  # SASA model
+class CNN_ATTN(nn.Module):
+    def __init__(self, configs):
+        super(CNN_ATTN, self).__init__()
+
+        self.conv_block1 = nn.Sequential(
+            nn.Conv1d(configs.input_channels, configs.mid_channels, kernel_size=configs.kernel_size,
+                      stride=configs.stride, bias=False, padding=(configs.kernel_size // 2)),
+            nn.BatchNorm1d(configs.mid_channels),
+            nn.ReLU(),
+            nn.MaxPool1d(kernel_size=2, stride=2, padding=1),
+            nn.Dropout(configs.dropout)
+        )
+
+        self.conv_block2 = nn.Sequential(
+            nn.Conv1d(configs.mid_channels, configs.mid_channels * 2, kernel_size=8, stride=1, bias=False, padding=4),
+            nn.BatchNorm1d(configs.mid_channels * 2),
+            nn.ReLU(),
+            nn.MaxPool1d(kernel_size=2, stride=2, padding=1)
+        )
+
+        self.conv_block3 = nn.Sequential(
+            nn.Conv1d(configs.mid_channels * 2, configs.final_out_channels, kernel_size=8, stride=1, bias=False,
+                      padding=4),
+            nn.BatchNorm1d(configs.final_out_channels),
+            nn.ReLU(),
+            nn.MaxPool1d(kernel_size=2, stride=2, padding=1),
+        )
+
+        self.adaptive_pool = nn.AdaptiveAvgPool1d(configs.features_len)
+        self.attn_network = attn_network(configs)
+        self.sparse_max = Sparsemax(dim=-1)
+        self.feat_len = configs.features_len
+
+    def forward(self, x_in):
+        x = self.conv_block1(x_in)
+        x = self.conv_block2(x)
+        x = self.conv_block3(x)
+        x = self.adaptive_pool(x)
+        x_flat = x.reshape(x.shape[0], -1)
+        attentive_feat = self.calculate_attentive_feat(x_flat)
+        return attentive_feat
+
+    def self_attention(self, Q, K, scale=True, sparse=True, k=3):
+
+        attention_weight = torch.bmm(Q.view(Q.shape[0], self.feat_len, -1), K.view(K.shape[0], -1, self.feat_len))
+
+        attention_weight = torch.mean(attention_weight, dim=2, keepdim=True)
+
+        if scale:
+            d_k = torch.tensor(K.shape[-1]).float()
+            attention_weight = attention_weight / torch.sqrt(d_k)
+        if sparse:
+            attention_weight_sparse = self.sparse_max(torch.reshape(attention_weight, [-1, self.feat_len]))
+            attention_weight = torch.reshape(attention_weight_sparse, [-1, attention_weight.shape[1],
+                                                                       attention_weight.shape[2]])
+        else:
+            attention_weight = self.softmax(attention_weight)
+
+        return attention_weight
+
+    def attention_fn(self, Q, K, scaled=False, sparse=True, k=1):
+
+        attention_weight = torch.matmul(F.normalize(Q, p=2, dim=-1),
+                                        F.normalize(K, p=2, dim=-1).view(K.shape[0], K.shape[1], -1, self.feat_len))
+
+        if scaled:
+            d_k = torch.tensor(K.shape[-1]).float()
+            attention_weight = attention_weight / torch.sqrt(d_k)
+            attention_weight = k * torch.log(torch.tensor(self.feat_len, dtype=torch.float32)) * attention_weight
+
+        if sparse:
+            attention_weight_sparse = self.sparse_max(torch.reshape(attention_weight, [-1, self.feat_len]))
+
+            attention_weight = torch.reshape(attention_weight_sparse, attention_weight.shape)
+        else:
+            attention_weight = self.softmax(attention_weight)
+
+        return attention_weight
+
+    def calculate_attentive_feat(self, candidate_representation_xi):
+        Q_xi, K_xi, V_xi = self.attn_network(candidate_representation_xi)
+        intra_attention_weight_xi = self.self_attention(Q=Q_xi, K=K_xi, sparse=True)
+        Z_i = torch.bmm(intra_attention_weight_xi.view(intra_attention_weight_xi.shape[0], 1, -1),
+                        V_xi.view(V_xi.shape[0], self.feat_len, -1))
+        final_feature = F.normalize(Z_i, dim=-1).view(Z_i.shape[0],-1)
+
+        return final_feature
+
+class attn_network(nn.Module):
+    def __init__(self, configs):
+        super(attn_network, self).__init__()
+
+        self.h_dim = configs.features_len * configs.final_out_channels
+        self.self_attn_Q = nn.Sequential(nn.Linear(in_features=self.h_dim, out_features=self.h_dim),
+                                         nn.ELU()
+                                         )
+        self.self_attn_K = nn.Sequential(nn.Linear(in_features=self.h_dim, out_features=self.h_dim),
+                                         nn.LeakyReLU()
+                                         )
+        self.self_attn_V = nn.Sequential(nn.Linear(in_features=self.h_dim, out_features=self.h_dim),
+                                         nn.LeakyReLU()
+                                         )
+
+    def forward(self, x):
+        Q = self.self_attn_Q(x)
+        K = self.self_attn_K(x)
+        V = self.self_attn_V(x)
+
+        return Q, K, V
+
+
+    # Sparse max
+class Sparsemax(nn.Module):
+    """Sparsemax function."""
+
+    def __init__(self, dim=None):
+        """Initialize sparsemax activation
+
+        Args:
+            dim (int, optional): The dimension over which to apply the sparsemax function.
+        """
+        super(Sparsemax, self).__init__()
+
+        self.dim = -1 if dim is None else dim
+
+    def forward(self, input):
+        """Forward function.
+        Args:
+            input (torch.Tensor): Input tensor. First dimension should be the batch size
+        Returns:
+            torch.Tensor: [batch_size x number_of_logits] Output tensor
+        """
+        # Sparsemax currently only handles 2-dim tensors,
+        # so we reshape to a convenient shape and reshape back after sparsemax
+        input = input.transpose(0, self.dim)
+        original_size = input.size()
+        input = input.reshape(input.size(0), -1)
+        input = input.transpose(0, 1)
+        dim = 1
+
+        number_of_logits = input.size(dim)
+
+        # Translate input by max for numerical stability
+        input = input - torch.max(input, dim=dim, keepdim=True)[0].expand_as(input)
+
+        # Sort input in descending order.
+        # (NOTE: Can be replaced with linear time selection method described here:
+        # http://stanford.edu/~jduchi/projects/DuchiShSiCh08.html)
+        zs = torch.sort(input=input, dim=dim, descending=True)[0]
+        range = torch.arange(start=1, end=number_of_logits + 1, step=1, device=input.device, dtype=input.dtype).view(1,
+                                                                                                                     -1)
+        range = range.expand_as(zs)
+
+        # Determine sparsity of projection
+        bound = 1 + range * zs
+        cumulative_sum_zs = torch.cumsum(zs, dim)
+        is_gt = torch.gt(bound, cumulative_sum_zs).type(input.type())
+        k = torch.max(is_gt * range, dim, keepdim=True)[0]
+
+        # Compute threshold function
+        zs_sparse = is_gt * zs
+
+        # Compute taus
+        taus = (torch.sum(zs_sparse, dim, keepdim=True) - 1) / k
+        taus = taus.expand_as(input)
+
+        # Sparsemax
+        self.output = torch.max(torch.zeros_like(input), input - taus)
+
+        # Reshape back to original shape
+        output = self.output
+        output = output.transpose(0, 1)
+        output = output.reshape(original_size)
+        output = output.transpose(0, self.dim)
+
+        return output
+
+    def backward(self, grad_output):
+        """Backward function."""
+        dim = 1
+
+        nonzeros = torch.ne(self.output, 0)
+        sum = torch.sum(grad_output * nonzeros, dim=dim) / torch.sum(nonzeros, dim=dim)
+        self.grad_input = nonzeros * (grad_output - sum.expand_as(grad_output))
+
+        return self.grad_input
