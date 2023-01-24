4d34e2a0e860821ecf80607188d3247e8709c977
Emadeldeen Eldele <37911596+emadeldeen24@users.noreply.github.com>
2023-01-24 14:16:52 +0800

    Update sweep_params.py

--- configs/sweep_params.py
+++ configs/sweep_params.py
@@ -73,5 +73,21 @@ sweep_alg_hparams = {
             'src_cls_loss_wt':  {'distribution': 'uniform', 'min': 1e-1, 'max': 10},
             'mmd_wt':           {'distribution': 'uniform', 'min': 1e-2, 'max': 10},
         },
+        
+        'SASA': {
+            'learning_rate':    {'values': [1e-2, 5e-3, 1e-3, 5e-4]},
+            'src_cls_loss_wt':  {'distribution': 'uniform', 'min': 1e-1, 'max': 10},
+            'domain_loss_wt':   {'distribution': 'uniform', 'min': 1e-2, 'max': 10},
+        },
+
+        'CoTMix': {
+            'learning_rate':            {'values': [1e-2, 5e-3, 1e-3, 5e-4]},
+            'temporal_shift':           {'values': [5, 10, 15, 20, 30, 50]},
+            'src_cls_weight':           {'distribution': 'uniform', 'min': 1e-1, 'max': 1},
+            'mix_ratio':                {'distribution': 'uniform', 'min': 0.5, 'max': 0.99},
+            'src_supCon_weight':        {'distribution': 'uniform', 'min': 1e-3, 'max': 1},
+            'trg_cont_weight':          {'distribution': 'uniform', 'min': 1e-3, 'max': 1},
+            'trg_entropy_weight':       {'distribution': 'uniform', 'min': 1e-3, 'max': 1},
+        },
 }
 
