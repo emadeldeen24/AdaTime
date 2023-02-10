5f8011a27bf37cb13d1323168be493065dc7e9bb
Emadeldeen Eldele <37911596+emadeldeen24@users.noreply.github.com>
2023-02-11 07:19:11 +0800

    Update data_model_configs.py

--- configs/data_model_configs.py
+++ configs/data_model_configs.py
@@ -4,7 +4,46 @@ def get_dataset_class(dataset_name):
         raise NotImplementedError("Dataset not found: {}".format(dataset_name))
     return globals()[dataset_name]
 
+class HAR():
+    def __init__(self):
+        super(HAR, self)
+        self.scenarios = [("2", "11"), ("6", "23"), ("7", "13"), ("9", "18"), ("12", "16"),
+                          ("18", "27"), ("20", "5"), ("24", "8"), ("28", "27"), ("30", "20")]
+        self.class_names = ['walk', 'upstairs', 'downstairs', 'sit', 'stand', 'lie']
+        self.sequence_len = 128
+        self.shuffle = True
+        self.drop_last = True
+        self.normalize = True
 
+        # model configs
+        self.input_channels = 9
+        self.kernel_size = 5
+        self.stride = 1
+        self.dropout = 0.5
+        self.num_classes = 6
+
+        # CNN and RESNET features
+        self.mid_channels = 64
+        self.final_out_channels = 128
+        self.features_len = 1
+
+        # TCN features
+        self.tcn_layers = [75, 150]
+        self.tcn_final_out_channles = self.tcn_layers[-1]
+        self.tcn_kernel_size = 17
+        self.tcn_dropout = 0.0
+
+        # lstm features
+        self.lstm_hid = 128
+        self.lstm_n_layers = 1
+        self.lstm_bid = False
+
+        # discriminator
+        self.disc_hid_dim = 64
+        self.hidden_dim = 500
+        self.DSKN_disc_hid = 128
+        
+        
 class EEG():
     def __init__(self):
         super(EEG, self).__init__()
