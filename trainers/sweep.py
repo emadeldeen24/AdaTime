6c8163a02998d4bbe0b9382f5587bc4f7951baf7
mohamedr002 <mohamedr002@e.ntu.edu.sg>
2023-05-16 07:47:20 +0800

    update

--- trainers/sweep.py
+++ trainers/sweep.py
@@ -79,7 +79,12 @@ class Trainer(AbstractTrainer):
 
                 # load data and train model
                 self.load_data(src_id, trg_id)
-                self.last_model, self.best_model = self.train_model()
+
+                # initiate the domain adaptation algorithm
+                self.initialize_algorithm()
+
+                # Train the domain adaptation algorithm
+                self.last_model, self.best_model = self.algorithm.update(self.src_train_dl, self.trg_train_dl, self.loss_avg_meters, self.logger)
 
                 # calculate metrics and risks
                 metrics = self.calculate_metrics()
@@ -100,42 +105,3 @@ class Trainer(AbstractTrainer):
         # finish the run
         run.finish()
 
-
-
-if __name__ == "__main__":
-    # ========= Select the DA methods ============
-    parser.add_argument('--da_method', default='Deep_Coral', type=str,
-                        help='DANN, Deep_Coral, WDGRL, MMDA, VADA, DIRT, CDAN, ADDA, HoMM, CoDATS')
-
-    # ========= Select the DATASET ==============
-    parser.add_argument('--data_path', default=r'../data', type=str, help='Path containing datase2t')
-    parser.add_argument('--dataset', default='HAR', type=str, help='Dataset of choice: (WISDM - EEG - HAR - HHAR_SA)')
-
-    # ========= Select the BACKBONE ==============
-    parser.add_argument('--backbone', default='CNN', type=str, help='Backbone of choice: (CNN - RESNET18 - TCN)')
-
-    # ========= Experiment settings ===============
-    parser.add_argument('--num_runs', default=3, type=int, help='Number of consecutive run with different seeds')
-    parser.add_argument('--device', default="cuda", type=str, help='cpu or cuda')
-
-    # ======== sweep settings =====================
-    parser.add_argument('--num_sweeps', default=1, type=str, help='Number of sweep runs')
-
-    # We run sweeps using wandb plateform, so next parameters are for wandb.
-    parser.add_argument('--sweep_project_wandb', default='ADATIME_refactor', type=str, help='Project name in Wandb')
-    parser.add_argument('--wandb_entity', type=str,
-                        help='Entity name in Wandb (can be left blank if there is a default entity)')
-    parser.add_argument('--hp_search_strategy', default="random", type=str,
-                        help='The way of selecting hyper-parameters (random-grid-bayes). in wandb see:https://docs.wandb.ai/guides/sweeps/configuration')
-    parser.add_argument('--metric_to_minimize', default="src_risk", type=str,
-                        help='select one of: (src_risk - trg_risk - few_shot_trg_risk - dev_risk)')
-
-    # ========  Experiments Name ================
-    parser.add_argument('--save_dir', default='experiments_logs/sweep_logs', type=str,
-                        help='Directory containing all experiments')
-
-    args = parser.parse_args()
-
-    trainer = Trainer(args)
-
-    trainer.train()
